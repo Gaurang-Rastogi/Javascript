020b27e5bed06f833e74c071b159733a8a11fdf8
Gaurang Rastogi <devilrastogi0@gmail.com>
2024-06-27 21:17:19 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code  as an new versio  of js.
+
+//  alert (3+3) we using the node.js not browser.
+
+
+console.log(3+
+    3    // we not use this pratices because of code readability.
+);
+
+let name="gaur" 
+let num= 3
+let isLoggedIn= false
+
+// number => 2 to power 53
+// bignit
+//string  => ""
+// boolean => true , false
+//null =>standalone value
+// undefined => value is not defined.
+// symbol=> unique
+
+// object
+
+console.log(typeof null); // type of tells the type of data. in case of null it shows  object.
