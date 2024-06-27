d6c1ff91b040636340a141660240e4c88d71ec62
Gaurang Rastogi <devilrastogi0@gmail.com>
2024-06-27 20:45:31 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = 12335;
+let accountEmail="gaurang2gmail.com";
+var accountPassword="234565";
+
+accountCity ="meerut"
+// accountId=22  not allowed
+accountEmail="develi6@gnail.com";
+accountPassword="89909";
+accountCity ="kanpur"
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountId])
+
+
+
+/*
+Prefernot to use var
+
+because of block scope issue and functional scope.
+*/
