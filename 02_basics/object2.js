9e86aa8ddb2e73bd13cf71636f7d602b4c2e01fd
Gaurang Rastogi <devilrastogi0@gmail.com>
2024-06-30 08:51:05 +0000

    object

--- 02_basics/object2.js
+++ 02_basics/object2.js
@@ -28,4 +28,24 @@ const obj2={3:"c",4:"d"}
 // const obj3= Object.assign({},obj1,obj2)   // {} asume this as a target and other as a source.
 
 const obj3 ={...obj1 , ...obj2}
-console.log(obj3);
+// console.log(obj3);
+
+
+
+// destructuring
+
+const course ={
+    course:"javascript",
+    price:"999",
+    duration: "2 months"
+}
+const {duration : courseDuration}=course
+//console.log(duration);
+console.log(courseDuration);
+
+
+// Json generally like a object only
+
+//  {"name":"gaurang",
+//    "price":"free"                 }
+         
