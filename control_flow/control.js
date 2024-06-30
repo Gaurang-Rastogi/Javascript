0d556cfa194359902a94e1cfcc408b99ebe83cb9
Gaurang Rastogi <devilrastogi0@gmail.com>
2024-06-30 12:27:58 +0000

    Control Flow

---
+++ control_flow/control.js
@@ -0,0 +1,53 @@
+// If
+
+// const isUserLoggedin =true
+// const temperature =42
+// if(temperature <50){
+//     console.log("temp less then 50");
+// }
+// console.log("temp greater then 50")
+
+// if else
+
+// if(temperature <50){
+//     console.log("temp less then 50");
+// }
+// else{
+//     console.log("temp greater then 50")
+
+// }
+
+// const score =200 
+//  if (score <100){
+//     let power ="fly"
+//     console.log(`User Power : ${power}`);
+//  }
+//  console.log(`User Power : ${power}`);
+
+
+//  netsing
+
+// const balance =1000
+
+// if(balance <500){
+//     console.log("less then 500 ")
+
+// }else if(balance <750){
+//     console.log("less the 750")
+// }else if(balance <900){
+//     console.log("less the 900")
+// }else if(balance <1200){
+//     console.log("less the 1200")
+// }
+
+const userLoggedIn = true
+const debitcard = true
+const loggedFromGoogle = false
+const loggedFromEmail = true
+
+if(userLogggedIn && debitcard){
+    console.log("allow the user")
+}
+if(loggedfromGoogle || LoggedEromEmail){
+    console.log("User Logged In")
+}
