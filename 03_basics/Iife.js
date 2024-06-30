9f85032ed1432f6d753fdbf8d6aede7fcf9585ab
Gaurang Rastogi <devilrastogi0@gmail.com>
2024-06-30 11:14:11 +0000

    IIFE

---
+++ 03_basics/Iife.js
@@ -0,0 +1,14 @@
+// Immediate Invoked function Expression (IIFE)
+
+// IIFE is used to prevent from the global scope pollution or variable
+
+(function chai(){
+    // named IIFe
+console.log(` IIFE explaination`);
+})();
+
+((name)=>{
+    console.log(`IIfe explaination Two , ${name}`);
+})("gaurang")
+
+// innamed IIfe
